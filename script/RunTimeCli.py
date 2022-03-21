79c28880d007f5e7d0f2d4d7a930536bec98551c
Mayank-124 <mayanknarang124@gmail.com>
2022-03-21 18:16:22 +0530

    removed garbage from the script

--- script/RunTimeCli.py
+++ script/RunTimeCli.py
@@ -1,5 +1,3 @@
-from email import message
-import time
 import serial
 
 ser = serial.Serial("COM14", 115200)
@@ -7,6 +5,3 @@ ser = serial.Serial("COM14", 115200)
 while 1:
     data = input("")
     ser.write(bytes(data, 'utf-8'))
-    time.sleep(1)
-
- 
