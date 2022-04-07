ab50ecae4a9b60f569eac11fa9d3d8e9e2174c11
Mayank-124 <mayanknarang124@gmail.com>
2022-04-07 15:08:48 +0530

    automated cli logs added

--- script/AutomatedCli/AutomatedCli.py
+++ script/AutomatedCli/AutomatedCli.py
@@ -1,34 +1,42 @@
 import time
 import serial
 
-cmd = ""
-response = ""
-
+f_log_ptr = open("cliLogs.txt","w")
 f_cmd_ptr = open("cliCommands.txt","r")
 serial_ptr = serial.Serial("COM6", 115200)
 
-def getResponse() :
-    global response
-    response = ""
-    while 1:
-        chr = serial_ptr.read(1).decode('utf-8')
-        if(chr == '$') :
-            break
-        else :
-            response += chr
+def putCommand():
+    cmd  = f_cmd_ptr.readline()
 
-while 1:
-    cmd = f_cmd_ptr.readline()
-
-    if(not cmd) :
-        break
+    if not cmd :
+        exit()
 
     if(cmd[-1] == '\n') :
         cmd = cmd[0:-1]
-
+    
     print(cmd)
     serial_ptr.write(bytes(cmd, 'utf-8'))
-    getResponse()
-    print(response, end="")
 
-    time.sleep(1)
+    f_log_ptr.write(cmd + '\n')
+
+def getResponse():
+    res = ""
+
+    while 1:
+        chr = serial_ptr.read(1).decode('utf-8')
+        if(chr == '$') :
+            print(res, end="")
+            break
+        else :
+            res += chr
+    
+    f_log_ptr.write(res)
+
+def doSomething() :
+
+    while 1 :    
+        putCommand()
+        getResponse()
+        time.sleep(1)
+
+doSomething()
