ddb1bdacd807904304751d49ad5c06968020e45b
Mayank-124 <mayanknarang124@gmail.com>
2022-04-17 23:10:54 +0530

    Run Time Cli Logs Added

--- script/RunTimeCli/RunTimeCli.py
+++ script/RunTimeCli/RunTimeCli.py
@@ -8,6 +8,7 @@ cliStopFrame = "bye bhai"
 UART_COMPORT  = sys.argv[1]
 UART_BAUDRATE = sys.argv[2]
 
+f_log_ptr = open("cliLogs.txt","w")
 ser = serial.Serial(UART_COMPORT, UART_BAUDRATE)
 
 def getResponse() :
@@ -15,6 +16,7 @@ def getResponse() :
     while 1:
         chr = ser.read(1).decode('utf-8')
         if(chr == '$') :
+            f_log_ptr.write(response)
             print(response, end="")
             break
         else :
@@ -22,6 +24,7 @@ def getResponse() :
 
 def doSomething() :
 
+    global f_log_ptr
     global isCmdMode
     global cliStartFrame
     global cliStopFrame
@@ -31,12 +34,15 @@ def doSomething() :
         ser.write(bytes(data, 'utf-8'))
 
         if(data == cliStartFrame) :
+            f_log_ptr = open("cliLogs.txt","w")
             isCmdMode = True
 
         if(isCmdMode) :
+            f_log_ptr.write(data + '\n')
             getResponse()
             
             if(data == cliStopFrame) :
+                f_log_ptr.close()
                 isCmdMode = False
 
 if __name__ == "__main__":
